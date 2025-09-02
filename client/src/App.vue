3f38c1165a5811ada2ad58c0f00f049b1a7172e6
ShridharUlli <shridharulli@gmail.com>
2025-09-02 23:14:29 +0530

    Revert "backend service"
    This reverts commit 22d142e1e5631547a9435bc0ddbb7c1187d0552c.

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="m-3">
+    <h1 class="text-4xl py-3 text-blue-600 font-bold">Hello world</h1>  
+    <span class="text-xl py-3 text-gray-500">{{ response }}</span>
+
+    <ul>
+      <li class="text-xl text-gray-500" v-for="person in persons" :key="persons.fname">
+        <span class="font-bold">{{ person.fname }}</span> .{{ person.lname }}
+      </li>
+    </ul>
+  </div>
+  
+</template>
+<script setup>
+import apiClient from './api/axios.js'
+import { ref, onMounted } from 'vue'
+import { NButton } from 'naive-ui'
+
+const persons = ref([])
+
+onMounted(()=>{
+  apiClient.get('/api/greet').then(res => {
+      persons.value = res.data      
+    }).catch(error =>{
+    })
+})
+
+</script>
+<style scoped>
+</style>
---
+++ client/src/api/axios.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+// const apiClient = axios.create({
+//     baseURL: 'http://localhost:8080',
+//     headers: {
+//     'Content-Type': 'application/json',
+//   },
+// })
+
+
+const apiClient = {
+  get: (url)=>{
+    return Promise.resolve({
+      data: [
+        {lname: "U", fname: 'Shridhar'},
+        {lname: "T", fname: 'Harshitha'},
+      ]
+    })
+  },
+  post:(url)=>{ Promise.resolve({data: {}})},
+  put:(url)=>{ Promise.resolve({data: {}})},
+  delete:(url)=>{ Promise.resolve({data: {}})},
+}
+export default apiClient;
